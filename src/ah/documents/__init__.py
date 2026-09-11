71049ebb1aaa24f4e2fa13fd29fc7154965ac666
MaxSquizie <ilya.savostov@bk.ru>
2026-09-11 19:43:53 +0300

    Expose document slice runtime diagnostics

--- src/ah/documents/__init__.py
+++ src/ah/documents/__init__.py
@@ -4,6 +4,7 @@ from .pipeline import (
     DocumentIngestionResult,
     DocumentProcessingError,
     DocumentProcessor,
+    DocumentSliceDiagnostic,
     DocumentSummary,
 )
 
@@ -13,5 +14,6 @@ __all__ = [
     "DocumentIngestionResult",
     "DocumentProcessingError",
     "DocumentProcessor",
+    "DocumentSliceDiagnostic",
     "DocumentSummary",
 ]
